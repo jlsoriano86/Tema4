a5a10d14a2918c1b4e0d176207a4fcab57af4f2e
jlsoriano86 <josesorna86@gmail.com>
2018-12-10 21:56:38 +0100

    Creación de la activity "Juego de Aciertos"

---
+++ app/src/main/java/com/example/pc/tema4/JuegoDeAciertosActivity.java
@@ -0,0 +1,13 @@
+package com.example.pc.tema4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JuegoDeAciertosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego_de_aciertos);
+    }
+}
